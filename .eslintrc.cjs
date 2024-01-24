cd22ac9c5153ba5d526b4d22eff153b270e1fb47
siko0001 <tom.sikora@etudiant.univ-reims.fr>
2024-01-24 09:09:54 +0100

    3. Qualité de code : ESLint et Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,25 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    "prettier",
+  ],
+  rules: {
+    "prettier/prettier": ["error"],
+  },
+};

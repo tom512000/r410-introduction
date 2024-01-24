35c453e6bae5352899ebd3f378b36536367434ee
siko0001 <tom.sikora@etudiant.univ-reims.fr>
2024-01-24 09:29:04 +0100

    5. Un premier composant

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from './components/Card.jsx'
 
 function App() {
   return (
@@ -6,7 +7,14 @@ function App() {
       <header className="app__header header">
         <h1 className="header__title">Introduction to React</h1>
       </header>
-      <main className="app__main">main</main>
+      <main className="app__main">
+        <div class="cards">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </main>
       <footer className="app__footer footer">footer</footer>
     </div>
   );
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Card() {
+  return (
+    <article className="card">
+      <header className="card__header header__title">Titre</header>
+      <section className="card__main">Content</section>
+    </article>
+  );
+}
+
+export default Card;

ebcfa2da14db3d2abeee26c901fa7f8aee03b84b
siko0001 <tom.sikora@etudiant.univ-reims.fr>
2024-01-24 09:02:18 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header header">
+        <h1 className="header__title">Introduction to React</h1>
+      </header>
+      <main className="app__main">main</main>
+      <footer className="app__footer footer">footer</footer>
+    </div>
+  );
+}
+
+export default App;
